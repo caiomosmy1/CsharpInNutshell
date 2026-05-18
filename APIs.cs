4ed00a680c712c28923003beca58749694a582a7
Caio Henrique <153246889+caiomosmy1@users.noreply.github.com>
2026-05-17 21:25:06 -0300

    Add outline for API design and management topics

---
+++ APIs.cs
@@ -0,0 +1,19 @@
+// 1 - Designing, Building, and Testing APIs
+
+// 2 - API Traffic Management
+
+// 3 - API Gateways: Ingress Traffic Management
+
+// 4 - Service Mesh
+
+// 5 - Deploying and Releasing APIs
+
+// 6 - Operational Security: Threat Modeling for APIs
+
+// 7 - API Authentication and Authetication
+
+// 8 - Redesingning Applications to API-Driven Architectures
+
+// 9 - Using API Infrastructure to Evolve Toward Cloud Platforms
+
+// 10 - Wrap-up
