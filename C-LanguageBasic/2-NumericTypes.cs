5cd5f3960f27d01d57bb57a735182435ec80c571
Caio Henrique <153246889+caiomosmy1@users.noreply.github.com>
2026-05-19 15:34:33 -0300

    Update 2-NumericTypes.cs

--- C-LanguageBasic/2-NumericTypes.cs
+++ C-LanguageBasic/2-NumericTypes.cs
@@ -1,6 +1,20 @@
 // CHAPTER 2 - c# LANGUAGE BASIC
 
 // 4-1 NUMERIC TYPES
+// The signed integral types are sbyte, short, int, long:
+int i = -1;
+i.Dump();
+
+// The unsigned integral types are byte, ushort, uint and ulong:
+byte b = 255;
+b.Dump();
+
+// The real types are float, double and decimal:
+double d = 1.23;
+d.Dump();
+
+// (See book for a table comparing each of the numeric types)
+
 // 4-2 NUMERIC LITERALS
 // 4-3 NUMERIC SUFFIXES
 // 4-4 NUMERIC CONVERSIONS
