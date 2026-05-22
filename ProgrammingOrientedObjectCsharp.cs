b7829269c1c8e2ad7bbf0e1d0c3b52eaa527855b
Caio Henrique <153246889+caiomosmy1@users.noreply.github.com>
2026-05-22 08:58:17 -0300

    Update ProgrammingOrientedObjectCsharp.cs

--- ProgrammingOrientedObjectCsharp.cs
+++ ProgrammingOrientedObjectCsharp.cs
@@ -3,7 +3,27 @@
 #### Processo a qual se isolam características de um objeto, considerando os que tenham em comum certos grupos de objetos.
 
 // ### REUSO
-// #### Herança, criando classes a partir de outra classe, aproveitando o código (dados ou comportamento) da camada classe mãe
+// #### Herança, criando classes a partir de outra classe, aproveitando o código (dados ou comportamento) da camada classe mãe*/
+
+public class Animal 
+{
+    public string Nome { get; set; }
+    
+    public void Alimentar() 
+    {
+        Console.WriteLine($"{Nome} está comendo.");
+    }
+}
+
+// A classe Cachorro reutiliza os dados e métodos de Animal
+public class Cachorro : Animal 
+{
+    public void Latir() 
+    {
+        Console.WriteLine("Au Au!");
+    }
+}
+
 
 // ESCAPSULAMENTO
 No caso, a complexidade que desejamos esconder é a de implemetação de alguma necessidade. Com o encapsulamento, podemos 
