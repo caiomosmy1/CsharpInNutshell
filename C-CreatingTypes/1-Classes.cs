aa6cac9616c1926d6d790d50d61c39ea73eed404
Caio Henrique <153246889+caiomosmy1@users.noreply.github.com>
2026-05-25 05:29:59 -0300

    Update 1-Classes.cs

--- C-CreatingTypes/1-Classes.cs
+++ C-CreatingTypes/1-Classes.cs
@@ -52,6 +52,101 @@ void Main()
   WriteCubes()
 }
 void WriteCubes()
+{
+  Console.WriteLine (Cube (3));
+  Console.WriteLine (Cube (5));
+  Console.WriteLine (Cube (4));
+
+  int Cube (int value) => value * value * value;
+}
+
+// Local methods - with top-level statements
+
+int x = 3;
+Foo();
+
+void Foo() => Console.WriteLine (X);
+
+// Methods - Overloading
+void Foo (int x)          { "int".Dump(); }
+void Foo (double x)       { "double".Dump(); }
+void Foo (int x, float y) { "int, float".Dump(); }
+void Foo (float x, int y) { "float, int".Dump(); }
+
+void Main()
+{
+  Foo (123);      // int
+  Foo (123.0);    // double
+  Foo (123, 123F);  // int, float
+  Foo (123F, 123);  // float, int
+}
+
+// The following overloads are prohibited:
+
+// Methods - Illegal Overloading
+
+void  Foo (int x);
+float Foo (int x);           // Compile-time error
+
+void  Goo (int[] x);
+void  Goo (params int[] x);  // Compile-time error
+
+void Hoo (int x);
+void Hoo (ref int x);      // OK so far
+void Hoo (out int x);      // Compile-time error
+
+void Main() {}
+
+//Constructors
+
+Panda p = new Panda ("Petey");   // Call constructor
+
+public class Panda
+{
+  string name;                   // Define field
+  public Panda (string n)        // Define constructor
+  {
+    name = n;                    // Initialization code (set up field)
+  }
+}
+
+Constructors - Overloading
+Constructors - Nonpublic
+Deconstructors
+Object Initializers
+Object Initializer Alternative - Optional Parameters
+The this Reference
+Properties
+Properties - calculated & read-only
+Properties - expression-bodied
+Automatic Properties
+Property Initializers
+Properties - get & set accessibility
+Indexers
+Primary Constructors
+Primary Constructors - additional constructors
+Primary Constructors and initializers
+Primary Constructors - masking
+Primary Constructors - validating
+Static Constructors
+Static Constructors & Field Initialization Order
+Static Constructors & Field Initialization Order (Constructor Call)
+Partial Types
+Partial Methods
+Extended Partial Methods
+The nameof operator
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
