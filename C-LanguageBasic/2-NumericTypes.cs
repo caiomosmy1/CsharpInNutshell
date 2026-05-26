fb91c80f41fef3857e53905e60738ffe82b8624e
Caio Henrique <153246889+caiomosmy1@users.noreply.github.com>
2026-05-26 07:48:40 +0000

    numericTypes

--- C-LanguageBasic/2-NumericTypes.cs
+++ C-LanguageBasic/2-NumericTypes.cs
@@ -16,13 +16,115 @@ d.Dump();
 // (See book for a table comparing each of the numeric types)
 
 // 4-2 NUMERIC LITERALS
+int x = 125;
+long y = 0x5F;
+
+int million = 2_000_000
+
+var b = ob1010_1011_1101_1110_1111;
+
+double d = 1.6;
+double doubleMillion = 1E06;
+
+Console.WriteLine( 1.0.GetType());
+Console.WriteLine(1E06.GetType());
+Console.WriteLine(1.GetType());
+Console.WriteLine(0xF000000.GetType());
+Console.WriteLine(0X1000000.GetType());
+
 // 4-3 NUMERIC SUFFIXES
+long i = 5;
+double x = 4.0;
+float f = 3.4f;
+decimal d = -1.23M;
+
 // 4-4 NUMERIC CONVERSIONS
+int x = 12345;
+long y = x;
+short z = (short)x;
+
+int i = 1;
+float f = i;
+
+int iExplicit = (int)f;
+
+int i1 = 100001;
+int i2 = i1;
+float = (int)f1;
+
 // 4-5 INCREMENT AND DECREMENT OPERATORS
+int x = 0, y = 0;
+Console.WriteLine (x++)
+Console.WriteLine (++y)
+
 // 4-6 INTEGRAL DIVISION
+int a = 2 / 3;
+
+int b = 0;
+int c = 5 / b;
+
 // 4-7 INTEGRAL OVERFLOW
+ int a = int.MinValue;
+ a--;
+ Console.WriteLine(a == inMaxValue);
+
+
 // 4-8 OVERFLOW CHECKING
+int a = 10000000;
+int b = 10000000;
+int c = checked (a * b);
+checked
+{
+    int c2 = a * b;
+    c2.Dump();
+}
 // 4-9 OVERFLOW CHECKING WITH CONSTANT EXPRESSIONS
+int x = int.MaxValue + 1;
+
+int y = unchecked (int.MaxValue + 1);
+
+
 // 4-10 /- AND 16-BIT LITERALS
+short x = 1, y = 1;
+short z = x + y;
+
+short z = (short) (x + y);
+
+
 // 4-11 SPECIAL FLOAT AND DOUBLE VALUES
+Console.WriteLine(double.NegativeInfinit);
+
+Console.WriteLine(1.0 / 0.0)
+Console.WriteLine(-1.0/ 0.0)
+Console.WriteLine(1.0 / -0.0)
+Console.WriteLine(-1.0 / -0.0)
+
+Console.WriteLine(0.0 / 0.0);
+Console.WriteLine((1.0 / 0.0) - (1.0 / 0.0 ));
+
+Console.WriteLine(0.0 / 0.0 == double.NaN);
+
+Console.WriteLine(double.isNaN (0.0 / 0.0));
+
+Console.WriteLine(object.Equals(0.0 / 0.0, double.NaN));
+
 // 4-12 REAL ROUDING ERROS
+{
+    float x = 0.1f;
+    Console.WriteLine(x + x + x + x + x + x + x + x + x + x);
+}
+{
+    decimal y =  0.1m;
+    Console.WriteLine (y + y + y + y + y + y + y + y + y + y);
+}
+
+decimal m = 1M / 6M;
+double d = 1.0 / 6.0;
+
+m.Dump ("m"); d.Dump ("d");
+
+decimal notQuiteWholeM = m+m+m+m+m+m
+double notQuiteWholeD = d+d+d+d+d+d
+
+Console.WriteLine (notQuiteWholeM == 1M);
+Console.WriteLine (notQuiteWholeD < 1.0);
