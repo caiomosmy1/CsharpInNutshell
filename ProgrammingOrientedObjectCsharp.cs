a258df77b7248a46962936c7ce50492262fd4971
Caio Henrique <153246889+caiomosmy1@users.noreply.github.com>
2026-05-27 23:10:16 -0300

    Atualizar o ProgrammingOrientedObjectCsharp.cs

--- ProgrammingOrientedObjectCsharp.cs
+++ ProgrammingOrientedObjectCsharp.cs
@@ -2,6 +2,11 @@
 ### Abstração
 #### Processo a qual se isolam características de um objeto, considerando os que tenham em comum certos grupos de objetos.
 
+
+
+
+
+
 // ### REUSO
 // #### Herança, criando classes a partir de outra classe, aproveitando o código (dados ou comportamento) da camada classe mãe*/
 
@@ -26,7 +31,6 @@ public class Cachorro : Animal
 
 
 // ESCAPSULAMENTO
-No caso, a complexidade que desejamos esconder é a de implemetação de alguma necessidade. Com o encapsulamento, podemos 
+/*No caso, a complexidade que desejamos esconder é a de implemetação de alguma necessidade. Com o encapsulamento, podemos 
 esconder a forma como algo foi feito, dadno a quem precisa apenas o resultado gerado, apena o resultado é relevante
-
 */
