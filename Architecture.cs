cf0cd396e90c2bd21fdae9b293956191d589a83b
Caio Henrique <153246889+caiomosmy1@users.noreply.github.com>
2026-05-29 07:49:13 -0300

    Add initial architecture overview comments

---
+++ Architecture.cs
@@ -0,0 +1,23 @@
+//Understand the basic
+
+// Responsibilities
+
+// Patterns & Design principles
+
+// Architecture
+
+// Security
+
+//Working with data
+
+// APIs & Integrations
+
+// Web, Mobile
+
+// Frameworks
+
+// Management
+
+//Network
+
+// Operations Knolowledge
